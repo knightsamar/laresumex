e706f3af6b28e600f421d8be3f08402c2428e14d
apoorva <apoorva@localhost>
2011-06-23 17:50:51 +0530

    DB changes

--- student_info/admin.py
+++ student_info/admin.py
@@ -2,9 +2,13 @@ from student_info.models import *
 from django.contrib import admin
 
 admin.site.register(student);
+admin.site.register(software_exposure);
+admin.site.register(ExtraField);
 admin.site.register(workex);
 admin.site.register(certification);
 admin.site.register(projects);
 admin.site.register(acedemic_acheivement);
 admin.site.register(extra_curricular);
+admin.site.register(personal);
+
 
--- student_info/models.py
+++ student_info/models.py
@@ -6,7 +6,9 @@ class student(models.Model):
     gender=(('m',"Male"),('f',"female"))
     
     prn = models.CharField(max_length=12,unique=True,primary_key=True);
-    fullname = models.CharField("Full Name", max_length=60, help_text="FULL NAME As on your certificates", blank=False,null=False)
+    fullname = models.CharField("First Name", max_length=60, help_text="FULL NAME As on your certificates", blank=False)
+    #middlename = models.CharField("middle Name", max_length=60, help_text="FULL NAME As on your certificates",)
+    #lastname = models.CharField("Last Name", max_length=60, help_text="FULL NAME As on your certificates", blank=False)
     sex=models.CharField(max_length=1,choices=gender);
     age=models.PositiveSmallIntegerField();
     email=models.EmailField(max_length=75);
@@ -20,25 +22,43 @@ class student(models.Model):
     Projects=models.BooleanField();
     acedemic_qualifications=models.BooleanField();
     Extra_curricular=models.BooleanField();
-   # Extra_field=models.NullBooleanField(null=True);
+    Extra_field=models.BooleanField();
    
     def __str__(self):
         return " Resume details of %s(%s)" % (self.fullname, self.prn);
  
 class marks(models.Model):
     primary_table=models.ForeignKey('student');
-    course=models.CharField(max_length=10, null=False);
-    uni=models.CharField(max_length=20);
+    course=models.CharField(max_length=30, null=False);
+    uni=models.CharField(max_length=100);
     marks=models.DecimalField(max_digits=5,decimal_places=2);
     year=models.PositiveIntegerField();
     
     def __str__(self):
         return "Obtained %d in %s at %s in year %d" % (self.marks,self.course,self.uni,self.year)
 class personal(models.Model):
-    primary_table=models.ForeignKey('student');
-    
+     primary_table=models.ForeignKey('student', null=False);
+     mother_name=models.CharField(max_length=20);
+     father_name=models.CharField(max_length=20);
+     mother_occupation=models.CharField(max_length=20);
+     father_occupation=models.CharField(max_length=20);
+     languages=models.CharField(max_length=100);
+     hobbies=models.CharField(max_length=100);
+     per_address=models.CharField(max_length=200);
+     corr_address=models.CharField(max_length=200);
+     def __str__(self):
+        return "Personal details about %s (%s)" % (self.primary_table.fullname, self.primary_table.prn);
 
- 
+class software_exposure(models.Model):
+    primary_table=models.ForeignKey('student', null=False);
+    programing_languages = models.CharField(max_length=100);
+    databases = models.CharField(max_length=100)
+    OS = models.CharField(max_length=100)
+    sw_packages = models.CharField(max_length=100)
+    web_tools = models.CharField(max_length=100)
+    def __str__(self):
+        return "Spftware Exposure of %s(%s)" % (self.primary_table.fullname, self.primary_table.prn);
+
 class ExtraField(models.Model):
     primary_table=models.ForeignKey('student');
     title=models.CharField(blank=False,max_length=20);
