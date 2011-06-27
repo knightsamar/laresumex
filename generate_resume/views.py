7051c8d027a8c4e373275001e29ad3b4d7468deb
knightsamar <knightsamar@ssi-knight>
2011-06-28 02:50:13 +0530

    Added tag UNTESTED for changeset 26e7f32b883a

---
+++ generate_resume/views.py
@@ -0,0 +1,116 @@
+# Create your views here.
+#''' will make comments which can auto-document this whole projects
+
+'''import data models '''
+from student_info.models import student;
+from generate_resume.models import resume; 
+
+''' import generator helpers '''
+from django.template import Context, loader
+from django.http import HttpResponse;
+from django.shortcuts import render_to_response;
+
+''' import vars '''
+from laresumex.settings import RESUME_STORE,RESUME_FORMAT
+
+''' import process helpers '''
+from subprocess import call #avail from python 2.6
+
+def latex(request,prn):
+    '''generates the resume and puts it into the resume store for version control'''
+    user = '10030142056' #the current user from session;
+    if prn is not None:
+        s = student.objects.get(pk=prn);
+        if s is not None:
+            #pass the student object with all his entered info to the template generator
+            t = loader.get_template('%s/template.tex' % RESUME_FORMAT);
+            c = Context({ 's' : s });
+            
+            resume_file = '%s%s.tex' % (RESUME_STORE,prn);
+
+            #now store the person's generated resumecode
+            f = file(resume_file,'w'); #if the file exists, overwrite it ELSE  create it.
+            f.write(t.render(c));
+            f.close();
+            
+            #now add this file to version control
+            try:
+                hg_command = "cd %s; hg add %s; hg commit -u laresumex -m 'updated by %s' " % (RESUME_STORE,resume_file,user);
+                return_status = get_done(hg_command);
+            except Exception as e:
+                print 'Exception was ', e;
+                response = HttpResponse("Some problem!");
+            finally:
+                if return_status is False:
+                    output = "<h3>Couldn't generate your .TEX file! Return code was %d </h3>" % return_status;
+                else:
+                    output = "<h3>Done!</h3>";
+        else:
+            output = "<h3>Student details for PRN %s not found!</h3>" % (prn);
+    else:
+       output = "<h3>Hey, pass me a PRN man!</h3>";
+   
+    return HttpResponse("hi");
+
+def pdf(request,prn):
+    if prn is not None:
+        s = student.objects.get(pk=prn);
+        if s is None:
+            output = "<h3>Student details for PRN %s not found!</h3>" % (prn);
+            response = HttpResponse(output);
+        #compare generate_resume.models.resume.last_tex_generated with student_info.models.student_last_updated and decide!
+        #is it fresher ?
+            #oh no! it isn't. generate it again!
+        
+        #ok, it is no need to regenerate
+            #call the generate_latex function with the prn
+
+        #if no record of this prn exists anywhere, tell the idiot!
+    
+    #generate it's pdf
+        pdf_file = "/tmp/%s.pdf" % (prn);
+        return_status = False;
+        #find the tex file
+        try:
+          #generate the pdf 
+          pdf_generation_command = "pdflatex --interaction=nonstopmode -etex -output-directory=/tmp %s%s.tex" % (RESUME_STORE,prn);
+          print str(pdf_generation_command).split();
+          return_status = call(pdf_generation_command.split())
+          print "Return status is ",return_status;
+          pdffile = "/tmp/%s.pdf" % prn;
+          resume_pdf = open(pdffile);
+          #prepare the file to be sent
+          response = HttpResponse(resume_pdf.read(), mimetype="application/pdf");
+          resume_pdf.close();
+          #name the file properly
+          response['Content-Disposition'] = "attachment; filename=SICSR_%s_resume.pdf" % s.fullname;
+        
+        except Exception as e:
+          response = HttpResponse("Some problem!");
+          print 'Exception was ', e;
+    ################because pdflatex can return 1 or 0 and still generate the file
+#          if return_status is not 0:
+#             output = "<h3>Couldn't generate your .PDF file! Return code was %s </h3>" % return_status;
+#          else:
+#             output = "<h3>Done!</h3>";
+    else:
+       output = "<h3>Hey, pass me a PRN man!</h3>";
+    
+    return response;
+
+def get_done(cmd):
+    '''handles all panga of executing a command on linux shell'''
+    print "Got total --> ", cmd
+    cmds = cmd.split(';'); #split multiple commands
+    for c in cmds:
+        try:
+           cmd_with_arguments=c.split();
+           print 'Executing ',cmd_with_arguments
+           r = call(cmd_with_arguments);
+           if r is not 0:
+               return False;    #no need of executing further commands
+        except Exception as e:
+          print 'Exception was ', e
+          return False;
+
+    return True;
---
+++ student_info/models.py
@@ -0,0 +1,100 @@
+from django.db import models
+
+# Create your models here.
+
+class student(models.Model):
+    gender=(('m',"Male"),('f',"female"))
+    
+    prn = models.CharField(max_length=12,unique=True,primary_key=True);
+    fullname = models.CharField("First Name", max_length=60, help_text="FULL NAME As on your certificates", blank=False)
+    sex=models.CharField(max_length=1,choices=gender);
+    age=models.PositiveSmallIntegerField();
+    email=models.EmailField(max_length=75);
+    phone_number=models.PositiveIntegerField(max_length=10);
+    career_objective=models.TextField(blank=False);
+    certification=models.BooleanField();
+    Projects=models.BooleanField();
+    academic_qualifications=models.BooleanField();
+    Extra_curricular=models.BooleanField();
+    Extra_field=models.BooleanField();
+   
+    def __str__(self):
+        return " Resume details of %s(%s)" % (self.fullname, self.prn);
+ 
+class marks(models.Model):
+    primary_table=models.ForeignKey('student');
+    course=models.CharField(max_length=30, null=False);
+    uni=models.CharField(max_length=100);
+    marks=models.DecimalField(max_digits=5,decimal_places=2);
+    year=models.PositiveIntegerField();
+    
+    def __str__(self):
+        return "Obtained %d in %s at %s in year %d" % (self.marks,self.course,self.uni,self.year)
+
+class personal(models.Model):
+     primary_table=models.ForeignKey('student', null=False);
+     mother_name=models.CharField(max_length=20);
+     father_name=models.CharField(max_length=20);
+     mother_occupation=models.CharField(max_length=20);
+     father_occupation=models.CharField(max_length=20);
+     languages=models.CharField(max_length=100);
+     hobbies=models.CharField(max_length=100);
+     per_address=models.CharField(max_length=200);
+     corr_address=models.CharField(max_length=200);
+     def __str__(self):
+        return "Personal details about %s (%s)" % (self.primary_table.fullname, self.primary_table.prn);
+
+class software_exposure(models.Model):
+    primary_table=models.ForeignKey('student', null=False);
+    programming_languages = models.CharField(max_length=100);
+    databases = models.CharField(max_length=100)
+    OS = models.CharField(max_length=100)
+    sw_packages = models.CharField(max_length=100)
+    web_tools = models.CharField(max_length=100)
+    def __str__(self):
+        return "Software Exposure of %s(%s)" % (self.primary_table.fullname, self.primary_table.prn);
+
+class ExtraField(models.Model):
+    primary_table=models.ForeignKey('student');
+    title=models.CharField(blank=False,max_length=20);
+    description = models.CharField(blank=False,max_length=100);
+    from_date = models.DateField();
+    end_date = models.DateField(null=True,blank=True);
+    def __str__(self):
+        return "Details about %s of %s" % (self.title,self.primary_table.fullname);
+
+class workex(ExtraField):
+    #title=models.CharField(default="workex", editable=False); -- think of a way to override the titile so that the title is always Eorkex.. or do we do that while taking the input???
+    pass;
+        
+class certification(ExtraField):
+    pass;
+
+class projects(ExtraField):
+    pass;
+
+class academic_acheivement(ExtraField):
+    pass;
+
+class extra_curricular(ExtraField):
+    pass;
+
+"""
+#In sabko ume actually use karna hai jab hum version treat karenge au
+
+class ExtraTable(models.Model):
+    tables = (('p','personal'),('sw','software_exposure'),('ex','extraField'),('m','marks'),('s','student'))
+    Column_Type = (('MVOLD','Multi-valued one line display'),
+                   ('MVMLD','Multi-valued multiple line display'),
+                   ('SVOLD','Single-valued one line display')
+                   );
+
+    column_name = Models.CharField(max_length=50);
+    column_type = Models.CharField(max_length=5,choices=Column_Type);
+    column_length = Models.PositiveIntegerField();
+    column_title = Models.CharField(max_length=2,choices=tables);
+
+class ExtraTableKaData(models.Model):
+    field = models.ForeignKey('ExtraTable');
+    data = models.TextField();
+"""
