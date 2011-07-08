b714a12837d62886ebf843eca2a21aa2d7b7a6ae
Samarendra M Hedaoo <opensource@fortyplustwo.net>
2011-07-08 20:41:27 +0530

    wrote a bit of code in the whole day which : 1. generates html form of resume .tex file(BUGGED) 2. executes commands inside a spec directory(TOBETESTED)w

---
+++ generate_resume/views.py
@@ -0,0 +1,154 @@
+# Create your views here.
+#''' will make comments which can auto-document this whole projects
+
+'''import data models '''
+from student_info.models import student;
+from generate_resume.models import resume;
+''' import generator helpers '''
+from django.template import Context, loader, RequestContext
+from django.http import HttpResponse;
+from django.shortcuts import render_to_response;
+
+''' import vars '''
+from laresumex.settings import RESUME_STORE,RESUME_FORMAT
+
+''' import process helpers '''
+from subprocess import call #avail from python 2.6
+from os import chdir #for changing directories
+
+def index(request):
+    # see whether user has logged in....
+    # if yes, see whether the user has already filled resume, then remove the create button.
+    # if no.. then remove the edit and the viw resume button.
+    '''
+    s=student.objects.filter(pk=prn);
+    if len(s) is 0:
+        it means there is no entry
+        create_form=True;
+    else:
+        Form already exists
+        create_form=False
+    c=Context({'create_form':create_form})
+    '''
+    t=loader.get_template('index.html')
+    c=Context();
+    return HttpResponse(t.render(c));
+
+
+def latex(request,prn):
+    '''generates the resume and puts it into the resume store for version control'''
+    user = '10030142056' #the current user from session;
+    if prn is not None:
+        s = student.objects.get(pk=prn);
+        if s is not None:
+            #pass the student object with all his entered info to the template generator
+            t = loader.get_template('%s/template.tex' % RESUME_FORMAT);
+            c = Context({ 's' : s });
+            
+            resume_file = '%s%s.tex' % (RESUME_STORE,prn);
+
+            #now store the person's generated resume-latex-code
+            f = file(resume_file,'w'); #if the file exists, overwrite it ELSE  create it.
+            f.write(t.render(c));
+            f.close();
+                    
+            #now add this file to version control
+            try:
+                hg_command = "hg add %s; hg commit -u laresumex -m 'updated by %s' " % (resume_file,user);
+                return_status = get_done(hg_command);
+            except Exception as e:
+                print 'Exception was ', e;
+            finally:
+                if return_status is False:
+                    output = "<h3>Couldn't generate your .TEX file! Return code was %d </h3>" % return_status;
+                else:
+                    output = "<h3>Done!</h3>";
+        else:
+            output = "<h3>Student details for PRN %s not found!</h3>" % (prn);
+    else:
+       output = "<h3>Hey, pass me a PRN man!</h3>";
+    
+    return HttpResponse(output);
+
+
+def pdf(request,prn):
+    if prn is not None:
+        s = student.objects.get(pk=prn);
+        if s is None:
+            output = "<h3>Student details for PRN %s not found!</h3>" % (prn);
+            response = HttpResponse(output);
+        #compare generate_resume.models.resume.last_tex_generated with student_info.models.student_last_updated and decide!
+        #is it fresher ?
+            #oh no! it isn't. generate it again!
+        
+        #ok, it is no need to regenerate
+            #call the generate_latex function with the prn
+
+        #if no record of this prn exists anywhere, tell the idiot!
+    
+    #generate it's pdf
+        pdf_file = "/tmp/%s.pdf" % (prn);
+        return_status = False;
+        #find the tex file
+        try:
+          #generate the pdf 
+          pdf_generation_command = "pdflatex --interaction=nonstopmode -etex -output-directory=/tmp %s%s.tex" % (RESUME_STORE,prn);
+          print str(pdf_generation_command).split();
+          return_status = call(pdf_generation_command.split())
+          print "Return status is ",return_status;
+          pdffile = "/tmp/%s.pdf" % prn;
+          resume_pdf = open(pdffile);
+          #prepare the file to be sent
+          response = HttpResponse(resume_pdf.read(), mimetype="application/pdf");
+          resume_pdf.close();
+          #name the file properly
+          response['Content-Disposition'] = "attachment; filename=SICSR_%s_resume.pdf" % s.fullname;
+        
+        except Exception as e:
+          response = HttpResponse("Some problem!");
+          print 'Exception was ', e;
+    ################because pdflatex can return 1 or 0 and still generate the file
+#          if return_status is not 0:
+#             output = "<h3>Couldn't generate your .PDF file! Return code was %s </h3>" % return_status;
+#          else:
+#             output = "<h3>Done!</h3>";
+    else:
+       output = "<h3>Hey, pass me a PRN man!</h3>";
+    
+    return response;
+
+def html(request,prn):
+    #is the text file current and updated? 
+
+    cmd = "yes 'Q' | htlatex 10030142031.tex"
+    if get_done(cmd,"%s/%s/" % (RESUME_STORE,prn)):
+        #show the HTML file
+        t = loader.get_template('/STORE/10030142031/');
+        c = Context();
+        return t.render(c);
+        
+
+
+
+
+def get_done(cmd,path=RESUME_STORE):
+    '''handles all panga of executing a command on linux shell'''
+    #where do we want to execute this ?
+    print "changing path to %s", path
+    chdir(path);
+
+    print "Got total --> ", cmd
+    cmds = cmd.split(';'); #split multiple commands
+    for c in cmds:
+        try:
+           cmd_with_arguments=c.split();
+           print 'Executing ',cmd_with_arguments
+           r = call(cmd_with_arguments);
+           if r is not 0:
+               return False;    #no need of executing further commands
+        except Exception as e:
+          print 'Exception was ', e
+          return False;
+
+    return True;
+
---
+++ student_info/models.py
@@ -0,0 +1,101 @@
+from django.db import models
+
+# Create your models here.
+
+class student(models.Model):
+    gender=(('m',"Male"),('f',"female"))
+    
+    prn = models.CharField(max_length=12,unique=True,primary_key=True);
+    fullname = models.CharField("First Name", max_length=60, help_text="FULL NAME As on your certificates", blank=False)
+    sex=models.CharField(max_length=1,choices=gender);
+    email=models.EmailField(max_length=75);
+    phone_number=models.PositiveIntegerField(max_length=10);
+    career_objective=models.TextField(blank=False);
+    certification=models.BooleanField();
+    project=models.BooleanField();
+    academic=models.BooleanField();
+    extracurricular=models.BooleanField();
+    Extra_field=models.BooleanField();
+   
+    def __str__(self):
+        return " Resume details of %s(%s)" % (self.fullname, self.prn);
+ 
+class marks(models.Model):
+    primary_table=models.ForeignKey('student');
+    course=models.CharField(max_length=30, null=False);
+    uni=models.CharField(max_length=100);
+    marks=models.DecimalField(max_digits=5,decimal_places=2);
+    fromDate=models.DateField(null=True, blank=True);
+    
+    def __str__(self):
+        return "Obtained %d in %s at %s" % (self.marks,self.course,self.uni)
+
+class personal(models.Model):
+     primary_table=models.ForeignKey('student', null=False, unique=True);
+     mother_name=models.CharField(max_length=20);
+     father_name=models.CharField(max_length=20);
+     birthdate=models.DateField(null=True);
+     mother_occupation=models.CharField(max_length=20); 
+     father_occupation=models.CharField(max_length=20);
+     languages=models.CharField(max_length=200);
+     hobbies=models.CharField(max_length=200);
+     strength=models.CharField(max_length=200);
+     per_address=models.TextField(max_length=200);
+     corr_address=models.TextField(max_length=200);
+     def __str__(self):
+        return "Personal details about %s (%s)" % (self.primary_table.fullname, self.primary_table.prn);
+class swExposure(models.Model):
+    primary_table=models.ForeignKey('student', null=False);
+    programming = models.CharField(max_length=100);
+    databases = models.CharField(max_length=100)
+    OS = models.CharField(max_length=100)
+    swPackages = models.CharField(max_length=100)
+    webTools = models.CharField(max_length=100)
+    def __str__(self):
+        return "Software Exposure of %s(%s)" % (self.primary_table.fullname, self.primary_table.prn);
+
+class ExtraField(models.Model):
+    primary_table=models.ForeignKey('student');
+    title=models.CharField(blank=False,max_length=20);
+    desc = models.CharField(blank=False,max_length=100);
+    fromDate = models.DateField(null=True,blank=True);
+    endDate = models.DateField(null=True,blank=True);
+    def __str__(self):
+        return "Details about %s -%s of %s" % (self.title,self.desc,self.primary_table.fullname);
+
+class workex(ExtraField):
+    #title=models.CharField(default="workex", editable=False); -- think of a way to override the titile so that the title is always Eorkex.. or do we do that while taking the input???
+    pass;
+        
+class certification(ExtraField):
+    pass;
+
+class project(ExtraField):
+    heading=models.CharField(max_length=40 ,blank=True);
+    
+
+class academic(ExtraField):
+    pass;
+
+class extracurricular(ExtraField):
+    pass;
+
+"""
+#In sabko ume actually use karna hai jab hum version treat karenge au
+
+class ExtraTable(models.Model):
+    tables = (('p','personal'),('sw','software_exposure'),('ex','extraField'),('m','marks'),('s','student'))
+    Column_Type = (('MVOLD','Multi-valued one line display'),
+                   ('MVMLD','Multi-valued multiple line display'),
+                   ('SVOLD','Single-valued one line display')
+                   );
+
+    column_name = Models.CharField(max_length=50);
+    column_type = Models.CharField(max_length=5,choices=Column_Type);
+    column_length = Models.PositiveIntegerField();
+    column_title = Models.CharField(max_length=2,choices=tables);
+
+class ExtraTableKaData(models.Model):
+    field = models.ForeignKey('ExtraTable');
+    data = models.TextField();
+"""
