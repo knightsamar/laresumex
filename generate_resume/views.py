eb69a416b565e2a5afb17392c5c4ac566854ab73
apoorva <apoorva@localhost>
2011-07-19 19:36:43 +0530

    kuch nahi kiya. bus template ka aur accha kiya.

--- generate_resume/views.py
+++ generate_resume/views.py
@@ -68,7 +68,7 @@ def latex(request,prn):
             student_data['p'] = student_data['p'][0]; #because we hv only one personal info row.
             student_data['sw']=student_data['sw'][0]
             #do we have the photo ? if yes, then include it.
-            student_data['photo'] = RESUME_STORE + "/photos/" + prn + ".png"  
+            student_data['photo'] = RESUME_STORE + "photos/" + prn + ".png"  
             #else, store None.
 
             pprint(student_data);
