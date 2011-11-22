04f72983a04f9a0305ee5be44549bf882c9a7309
apoorva <apoorva@localhost>
2011-11-23 01:33:26 +0530

    final changes.. everything working. i hope ;)

---
+++ company/admin.py
@@ -0,0 +1,86 @@
+from company.models import *
+from django.contrib import admin
+from student_info.models import student; #for mailing the student on his personal mail address.
+from django.core.mail import EmailMessage,get_connection #because this one actually let's use BCC and all.
+from django.template import Context, loader
+from laresumex.settings import MANAGERS;
+'''
+class membershipInline(admin.TabularInline):
+    model = student_info.placement_in.set; #a reference to the intermediary modeli
+    extra = 1
+#    fk_name = 'placement_in'
+'''
+
+class companyAdmin(admin.ModelAdmin):
+    __name__='CompanyAdmin';
+    readonly_fields = ['students_applied']; #will display this field as a link rather than editable box
+    
+    #for list display
+    list_display = ('name','date_of_process','last_date_of_applying')
+    list_filter = ['came_for_group'];
+    #inlines = [
+    #    membershipInline,
+    #    ]
+    
+    #add a set of actions
+    actions = ['informStudents'];
+
+    def informStudents(self, request, selectedCompanies):
+        print "Informing students about ",selectedCompanies;
+
+        t = loader.get_template('company/informStudents_mail.html');
+        conn = get_connection(); #for mailing
+        
+        
+        for c in selectedCompanies:
+            ppltoInform = c.came_for_group.get_query_set()[0].user_set.all() 
+            context = Context(
+                {
+                    'company' : c,
+                })
+
+            body = t.render(context)
+
+            to = [];
+            for p in ppltoInform:
+                to += ["%s@sicsr.ac.in" % str(p.username)]; #for primary sicsrwala address
+        
+            #send mail actually.
+            email = EmailMessage();
+            email.from_email = '"Ashwini Narayan" <placements@sicsr.ac.in>';
+            email.subject = '[Placements] %s coming to campus' % c.name;
+            #email.from = 'root@sdrcserver.sdrc'; #left for automatic putting
+            email.connection = conn;
+            email.body = body;
+            
+            cc = [];
+            # Getting the From/ CC from the placement Committee group.
+            from django.contrib.auth.models import Group;
+            g = Group.objects.get(name = 'placement committee')
+            
+            for u in g.user_set.all():
+               cc.append(u.email)
+            
+            bcc = []
+            for m in MANAGERS:
+                bcc.append(list(m)[1])
+            
+            email.to = to + cc;
+            email.bcc = bcc;
+            print "to is ",email.to;
+            print "bcc is ",email.bcc;
+            print "body is ",email.body
+            email.content_subtype='html';
+            email.send();
+            dir(email)
+            print "Done with %s" % (c);
+
+        self.message_user(request,"Successfully informd students");    
+    informStudents.short_description = "Inform students about companies"
+
+class placement_inAdmin(admin.ModelAdmin):
+    list_filter = ['company']
+
+admin.site.register(company,companyAdmin);
+admin.site.register(placement_in,placement_inAdmin);
+
---
+++ urls.py
@@ -0,0 +1,64 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^laresumex/', include('laresumex.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+     (r'^admin/', include(admin.site.urls)),
+
+    # actually our urls START FROM NOW
+
+    #contact
+    #(r'^/$',"generate_resume.views.index"),   
+    (r'^common/(?P<msg>\D+)/done',"common.views.done"),
+    (r'^contact',"common.views.contact"),
+    
+    #jobposting
+    
+    (r'^jobposting/add$',"jobposting.views.add"),
+    (r'^jobposting/view_hidden$',"jobposting.views.hidden"),
+    (r'^jobposting/(?P<template>\D+)/view$',"jobposting.views.view"),
+    (r'^jobposting/(?P<template>\D+)/do$',"jobposting.views.do"),
+
+    # generate_resume
+    (r'^generate_resume/(?P<prn>\d+)/pdf',"generate_resume.views.pdf"),
+    (r'^generate_resume/(?P<prn>\d+)/html',"generate_resume.views.html"),
+    #(r'^generate_resume/(?P<prn>\d+)/latex',"generate_resume.views.latex"),
+    
+    #ldap_login
+    (r'^ldap_login/$','ldap_login.views.login'), #for authentication
+    (r'^ldap_login/logout$','ldap_login.views.logout'), #for loggin out
+    (r'^home/$',"common.views.index"),
+    (r'^$','common.views.index'),
+    
+    # company
+    (r'^company/getResume',"company.views.getResume"),
+    (r'^company/list',"company.views.company_list"),
+    (r'^company/get_student_name',"company.views.get_students_name"),
+    (r'^company/apply',"company.views.apply"),
+    (r'^PT/admin',"company.views.admin_index"),
+    (r'^PT/fetch',"company.views.fetch_index"),
+    (r'^PT/reports',"company.views.got_placed"),
+    
+    
+    # student_info
+    (r'^student_info/(?P<prn>\d+)/edit',"student_info.views.edit"),
+    (r'^student_info/form',"student_info.views.showform"),
+    (r'^student_info/(?P<prn>\d+)/submit',"student_info.views.submit"),
+    (r'^form',"student_info.views.showform"),
+
+    #trying out django social auth
+    #(r'^socialauth/sample/',"socialauth.views.sampleView"),
+    #(r'^socialauth/login-handler/',"socialauth.views.loginHandler"),
+    #(r'^socialauth/logout/',"socialauth.views.logout"), 
+    #url(r'', include('social_auth.urls')),
+    
+)
